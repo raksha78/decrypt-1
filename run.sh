a22092a2a8ec61e65664db29a3185db00b9baf9e
Chandrakanth <chandrakanth@shippable.com>
2017-07-26 14:23:17 +0530

    final working

--- run.sh
+++ run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash/ -e
-keys=$(cat config.json | jq '.'config.environment.testing.secureKeys | jq 'keys')
-
-shell_array_of_keys=$(echo $keys | jq -r '.[]')
-
-for key in $shell_array_of_keys; do
+keys=$(cat config.json | jq '.'config.environment.testing.secureKeys | jq 'keys' | jq -r '.[]')
+#shell_array_of_keys=$(echo $keys | jq -r '.[]')
+for key in $keys; do
   value=$(cat config.json | jq -r '.'config.environment.testing.secureKeys.$key)
   echo $value > encrypted.txt
   shippable_decrypt "encrypted.txt"
-  echo \n >> encrypted.txt.decrypted
-  cat encrypted.txt.decrypted
+  echo >> encrypted.txt.decrypted
 done;
 
 cat encrypted.txt.decrypted
 source encrypted.txt.decrypted
+rm -rf encrypted.txt.decrypted
